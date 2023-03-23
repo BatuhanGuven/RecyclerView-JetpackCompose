b887227ddb3bd17f210731335deaa369fc51265f
BatuhanGuven <batuhanguven3806@gmail.com>
2023-03-23 15:22:10 +0100

    RecyclerView

---
+++ app/src/main/java/com/example/recyclerview_jetpackcompose/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.recyclerview_jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.recyclerview_jetpackcompose.ui.theme.RecyclerViewJetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp()
+        }
+    }
+}
+@Composable
+fun MyApp() {
+    BarkHomeContent()
+}
